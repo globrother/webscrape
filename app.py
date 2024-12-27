6d8eac688d50b56debf8c1cff2d609c38c87e81e
globrother <clebergobira@yahoo.com.br>
2024-12-27 14:01:26 -0300

    implementando FIIs

--- app.py
+++ app.py
@@ -1,47 +1,45 @@
 from flask import Flask, request, jsonify
-import ask_sdk_core as ask_sdk
 import requests
 from bs4 import BeautifulSoup
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_request_type, get_supported_interfaces
+from ask_sdk_model.interfaces.alexa.presentation.apl import RenderDocumentDirective
 
 app = Flask(__name__)
 
 def get_element():
     url = 'https://statusinvest.com.br/fundos-imobiliarios/xpml11'
-    # Definindo um header User-Agent
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
     
     response = requests.get(url, headers=headers)
-    # Verificando o status da resposta
     if response.status_code == 200:
-        # Criando um objeto BeautifulSoup a partir do conteúdo HTML
         soup = BeautifulSoup(response.content, 'html.parser')
-        # Encontrando a tag <div> com a classe "container pb-7"
         container_divs = soup.find_all('div', class_='container pb-7')
-        # Tags que você deseja capturar
-        tags = ['value'] # Pode adicionar mais tags se necessário
+        tags = ['value']
         
         xpml11_0 = dyxpml11_3 = pvpxpml11_6 = divpcxmpl11_16 = None
         
-        # Iterando sobre cada <div> encontrado
         for div in container_divs:
             for tag in tags:
                 elements = div.find_all(class_=tag)
-                # Garantindo que há pelo menos 5 elementos na lista
                 if len(elements) > 4:
                     xpml11_0 = elements[0].text
                     dyxpml11_3 = elements[3].text
                     pvpxpml11_6 = elements[6].text
                     divpcxmpl11_16 = elements[15].text
                     break
-        # Tratamento de Erros: Verifica se todos os elementos foram encontrados
+        
         if not all([xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16]):
             raise ValueError("Unable to scrape all required elements.")
     else:
         raise ConnectionError(f"Erro ao acessar o site: Status Code {response.status_code}")
 
     return xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16
+
+APL_DOCUMENT_TOKEN = "documentToken"
 
 @app.route('/webscrape', methods=['POST'])
 def alexa():
@@ -59,7 +57,6 @@ def alexa():
                           f"• P/VP: {pvpxpml11_6}\n" \
                           f"• Último rendimento: R$ {divpcxmpl11_16}"
 
-        # Documento APL para a imagem de fundo
         apl_document = {
             "type": "APL",
             "version": "2024.3",
@@ -105,7 +102,7 @@ def alexa():
                                         "items": [
                                             {
                                                 "type": "Text",
-                                                "text": marcadores_card,
+                                                "text": marcadores_voz,
                                                 "style": "textStyleDisplay4",
                                                 "textAlign": "left",
                                                 "speech": "${payload.longTextTemplateData.properties.plantInfoSpeech}",
@@ -129,36 +126,68 @@ def alexa():
             ]
         }
 
+        datasources = {
+            "longTextTemplateData": {
+                "type": "object",
+                "objectId": "longTextSample",
+                "properties": {
+                    "backgroundImage": {
+                        "contentDescription": None,
+                        "smallSourceUrl": None,
+                        "largeSourceUrl": None,
+                        "sources": [
+                            {
+                                "url": "https://www.hashtagtreinamentos.com/wp-content/uploads/2022/02/Capa-Para-Dashboard-no-Power-BI-3.png",
+                                "size": "large"
+                            }
+                        ]
+                    },
+                    "title": "Atualizações Fundos Imobiliários - FIIs",
+                    "textContent": {
+                        "primaryText": {
+                            "type": "PlainText",
+                            "text": marcadores_card
+                        }
+                    },
+                    "logoUrl": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/logo/logo-modern-botanical-white.png",
+                    "plantSpeechSSML": f"<speak>{marcadores_voz}</speak>"
+                },
+                "transformers": [
+                    {
+                        "inputPath": "plantSpeechSSML",
+                        "transformer": "ssmlToSpeech",
+                        "outputName": "plantInfoSpeech"
+                    }
+                ]
+            }
+        }
+
         response = {
-            
-            #"version": "1.0",
-            #"response": {
-            #    "outputSpeech": {
-            #        "type": "SSML",
-            #        "ssml": f"<speak>Atualizações do Fundo XPML onze \n{marcadores_voz}</speak>"
-            #    },
-            #    "card": {
-            #        "type": "Simple",
-            #        "title": "Obtendo de Status Invest",
-            #        "content": f"Atualizações do Fundo XPML11:\n{marcadores_card}"
-            #    },
-            #    "shouldEndSession": True
-            #},
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "SSML",
+                    "ssml": f"<speak>Atualizações do Fundo XPML onze \n{marcadores_voz}</speak>"
+                },
+                "card": {
+                    "type": "Simple",
+                    "title": "Obtendo de Status Invest",
+                    "content": f"Atualizações do Fundo XPML11:\n{marcadores_card}"
+                },
+                "shouldEndSession": True
+            },
             "directives": [
                 {
                     "type": "Alexa.Presentation.APL.RenderDocument",
-                    "token": "apl_document",
-                    "document": {
-                        "src": "doc://alexa/apl/documents/Alexawebscrape",
-                        "type": "Link"
-                    },
+                    "token": APL_DOCUMENT_TOKEN,
+                    "document": apl_document,
+                    "datasources": datasources
                 }
             ]
         }
         return jsonify(response)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
