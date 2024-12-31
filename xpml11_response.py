9bd1baaa08b09731442adaaf336a294d6464d65c
globrother <clebergobira@yahoo.com.br>
2024-12-31 10:21:03 -0300

    implementando FIIs

--- xpml11_response.py
+++ xpml11_response.py
@@ -11,9 +11,9 @@ def alexa_xpml11(get_element, request, requests, BeautifulSoup):
         xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16 = get_element(requests, BeautifulSoup)
         
         voz_xpml11 = f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>" \
-             f"• Dividend Yield: {dyxpml11_3}%\n<break time='500ms'/>" \
-             f"• P/VP: {pvpxpml11_6}\n<break time='500ms'/>" \
-             f"• Último rendimento: R$ {divpcxmpl11_16}<break time='500ms'/>"
+             f"• Dividend Yield: {dyxpml11_3}%\n" \
+             f"• P/VP: {pvpxpml11_6}\n" \
+             f"• Último rendimento: R$ {divpcxmpl11_16}"
         
         card_xpml11 = f"• Valor atual da cota: R$ {xpml11_0}\n" \
              f"• Dividend Yield: {dyxpml11_3}%\n" \
@@ -24,8 +24,8 @@ def alexa_xpml11(get_element, request, requests, BeautifulSoup):
             "version": "1.0",
             "response": {
                 "outputSpeech": {
-                    "type": "PlainText",
-                    "text": f"\n{voz_xpml11}"
+                    "type": "SSML",
+                    "text": f"<speak> Atualizações do Fundo XPML onze \n{voz_xpml11}</speak>"
                 },
                 "card": {
                     "type": "Simple",
