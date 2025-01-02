9810f3c79369762a3e82b8a2e8a53de19be1cf76
globrother <clebergobira@yahoo.com.br>
2025-01-02 16:19:41 -0300

    implementando apl

--- app.py
+++ app.py
@@ -5,20 +5,147 @@ from bs4 import BeautifulSoup
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type, get_supported_interfaces
-from ask_sdk_model.interfaces.alexa.presentation.apl import RenderDocumentDirective
-from threading import Timer
-from xpml11_response import alexa_xpml11
-from xpml11 import get_element
+from ask_sdk_model.interfaces.alexa.presentation.apl import (
+    RenderDocumentDirective, ExecuteCommandsDirective)
+import logging
+#from threading import Timer
+#from xpml11_response import alexa_xpml11
+#from xpml11 import get_element
 
 # from xpml11 import get_element
 # from xpml11_response import get_element
 
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model import Response
+from ask_sdk_model.interfaces.alexa.presentation.apl import (
+    RenderDocumentDirective, ExecuteCommandsDirective)
+from ask_sdk_model.ui import SimpleCard
+import logging
 
-@app.route("/webscrape", methods=["POST"])
-def handle_request():
-    
-    return alexa_xpml11(json,get_element, request, requests, BeautifulSoup)
+from flask import Flask, request, jsonify
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model import Response
+from ask_sdk_model.interfaces.alexa.presentation.apl import (
+    RenderDocumentDirective, ExecuteCommandsDirective)
+from ask_sdk_model.ui import SimpleCard
+import logging
+
+app = Flask(__name__)
+sb = SkillBuilder()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        logger.info("LaunchRequestHandler can_handle invoked")
+        return handler_input.request_envelope.request.object_type == "LaunchRequest"
+
+    def handle(self, handler_input):
+        logger.info("LaunchRequestHandler handle invoked")
+        speech_text = "Bem-vindo! Aqui estão os cartões."
+        
+        apl_document = {
+            "type": "APL",
+            "version": "1.4",
+            "mainTemplate": {
+                "items": [
+                    {
+                        "type": "Container",
+                        "items": [
+                            {
+                                "type": "Image",
+                                "source": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf",
+                                "width": "100vw",
+                                "height": "100vh"
+                            },
+                            {
+                                "type": "Text",
+                                "id": "textComponent",
+                                "text": "Primeiro cartão",
+                                "fontSize": "50dp",
+                                "color": "#FFFFFF",
+                                "textAlign": "center",
+                                "textAlignVertical": "center"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        
+        apl_command = [
+            {
+                "type": "SpeakItem",
+                "componentId": "textComponent",
+                "highlightMode": "line"
+            },
+            {
+                "type": "SetValue",
+                "componentId": "textComponent",
+                "property": "text",
+                "value": "Segundo cartão"
+            },
+            {
+                "type": "SpeakItem",
+                "componentId": "textComponent",
+                "highlightMode": "line"
+            }
+        ]
+
+        handler_input.response_builder.add_directive(
+            RenderDocumentDirective(
+                token="uniqueToken",
+                document=apl_document
+            )
+        ).add_directive(
+            ExecuteCommandsDirective(
+                token="uniqueToken",
+                commands=apl_command
+            )
+        ).speak(speech_text).set_card(SimpleCard("Cartões", speech_text)).set_should_end_session(False)
+        
+        return handler_input.response_builder.response
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return handler_input.request_envelope.request.object_type == "IntentRequest"
+
+    def handle(self, handler_input):
+        speech_text = "Desculpe, não entendi sua solicitação. Você pode tentar novamente?"
+        
+        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+        
+        return handler_input.response_builder.response
+
+class ErrorHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        return True
+
+    def handle(self, handler_input, exception):
+        logger.error(exception, exc_info=True)
+        speech_text = "Desculpe, ocorreu um erro ao processar sua solicitação."
+        
+        handler_input.response_builder.speak(speech_text).set_should_end_session(True)
+        
+        return handler_input.response_builder.response
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_exception_handler(ErrorHandler())
+
+@app.route("/webscrape", methods=['POST'])
+def webscrape():
+    logger.info("Received request at /webscrape endpoint")
+    request_body = request.json
+    response = sb.lambda_handler()(request_body, None)
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
--- xpml11_response.py
+++ xpml11_response.py
@@ -5,123 +5,110 @@ from flask import jsonify
 #import time
 #from xpml11 import get_element
 
-def alexa_xpml11(json, get_element, request, requests, BeautifulSoup):
-    try:
+#def alexa_xpml11(sb, ExecuteCommandsDirective, RenderDocumentDirective, AbstractRequestHandler, json, get_element, request, requests, BeautifulSoup):
+   # try:
         #data = request.get_json()
-        request_data = request.get_json()
+        #request_data = request.get_json()
 
         # Adiciona instrução de depuração para imprimir o conteúdo da solicitação
         # print("Request Data:", json.dumps(request_data, indent=2))
 
-        xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16 = get_element(requests, BeautifulSoup)
+        #xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16 = get_element(requests, BeautifulSoup)
         
-        voz_xpml11 = (
-            f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
-            f"• Dividend Yield: {dyxpml11_3}%\n<break time='500ms'/>"
-            f"• PVP: {pvpxpml11_6}\n<break time='500ms'/>"
-            f"• Último rendimento: R$ {divpcxmpl11_16}<break time='1s'/>"
-        )
+        #voz_xpml11 = (
+            #f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
+            #f"• Dividend Yield: {dyxpml11_3}%\n<break time='500ms'/>"
+            #f"• PVP: {pvpxpml11_6}\n<break time='500ms'/>"
+            #f"• Último rendimento: R$ {divpcxmpl11_16}<break time='1s'/>"
+        #)
         
-        card_xpml11 = (
-            f"• Valor atual da cota: R$ {xpml11_0}<br>"
-            f"• Dividend Yield: {dyxpml11_3}%<br>"
-            f"• P/VP: {pvpxpml11_6}<br>"
-            f"• Último rendimento: R$ {divpcxmpl11_16}"
-        )
+        #card_xpml11 = (
+           # f"• Valor atual da cota: R$ {xpml11_0}<br>"
+           # f"• Dividend Yield: {dyxpml11_3}%<br>"
+          #  f"• P/VP: {pvpxpml11_6}<br>"
+          #  f"• Último rendimento: R$ {divpcxmpl11_16}"
+        #)
 
-        voz_teste1 = (
-            f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
-        )
+       # voz_teste1 = (
+          #  f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
+       # )
 
-        card_teste1 = (
-            "• Valor atual da cota: R$ 5.000,00<br>"
-        )
+        #card_teste1 = (
+        #    "• Valor atual da cota: R$ 5.000,00<br>"
+        #)
 
         # Verifica o tipo de solicitação da Alexa
-        if request_data["request"]["type"] in ["LaunchRequest", "IntentRequest"]:
-            response = {
-                "version": "1.0",
-                "response": {
-                    "outputSpeech": {
-                        "type": "SSML",
-                        "ssml": f"<speak>Atualizações do Fundo XPML11. {voz_xpml11}</speak>"
-                    },
-                    "directives": [
-                        {
-                            "type": "Alexa.Presentation.APL.RenderDocument",
-                            "token": "welcomeToken",
-                            "document": {
-                                "type": "APL",
-                                "version": "1.4",
-                                "mainTemplate": {
-                                    "items": [
-                                        {
-                                            "type": "Container",
-                                            "height": "100vh",
-                                            "items": [
-                                                {
-                                                    "type": "Image",
-                                                    "source": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf",
-                                                    "width": "100vw",
-                                                    "height": "100vh"
-                                                },
-                                                {
-                                                    "type": "Text",
-                                                    "text": f"Atualizações do Fundo XPML11:<br><br>{card_xpml11}",
-                                                    "style": "textStyleDisplay4",
-                                                    "textAlign": "left",
-                                                    "id": "financeContent-xpml"
-                                                },
-                                                {
-                                                    "type": "Text",
-                                                    "text": f"{card_teste1}",
-                                                    "style": "textStyleDisplay4",
-                                                    "textAlign": "left",
-                                                    "id": "financeContent-teste1",
-                                                    "display": "none"
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                }
+        class LaunchRequestHandler(AbstractRequestHandler):
+            def can_handle(self, handler_input):
+                return handler_input.request_envelope.request.object_type == "LaunchRequest"
+
+            def handle(self, handler_input):
+                speech_text = "Bem-vindo! Aqui estão os cartões."
+                
+                apl_document = {
+                    "type": "APL",
+                    "version": "1.4",
+                    "mainTemplate": {
+                        "items": [
+                            {
+                                "type": "Container",
+                                "items": [
+                                    {
+                                        "type": "Image",
+                                        "source": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf",
+                                        "width": "100vw",
+                                        "height": "100vh"
+                                    },
+                                    {
+                                        "type": "Text",
+                                        "text": "Primeiro cartão",
+                                        "fontSize": "50dp",
+                                        "color": "#FFFFFF",
+                                        "textAlign": "center",
+                                        "textAlignVertical": "center"
+                                    }
+                                ]
                             }
-                        }
-                    ],
+                        ]
+                    }
+                }
+                
+                apl_command = {
+                    "type": "Sequential",
                     "commands": [
                         {
-                            "type": "Sequential",
-                            "commands": [
-                                {
-                                    "type": "SetValue",
-                                    "componentId": "financeContent-xpml",
-                                    "property": "display",
-                                    "value": "normal"
-                                },
-                                {
-                                    "type": "SpeakItem",
-                                    "componentId": "financeContent-xpml",
-                                    "highlightMode": "line",
-                                    "align": "center"
-                                },
-                                {
-                                    "type": "SetValue",
-                                    "componentId": "financeContent-teste1",
-                                    "property": "display",
-                                    "value": "normal"
-                                },
-                                {
-                                    "type": "SpeakItem",
-                                    "componentId": "financeContent-teste1",
-                                    "highlightMode": "line",
-                                    "align": "center"
-                                }
-                            ]
+                            "type": "SpeakItem",
+                            "componentId": "textComponent",
+                            "highlightMode": "line"
+                        },
+                        {
+                            "type": "SetValue",
+                            "componentId": "textComponent",
+                            "property": "text",
+                            "value": "Segundo cartão"
+                        },
+                        {
+                            "type": "SpeakItem",
+                            "componentId": "textComponent",
+                            "highlightMode": "line"
                         }
                     ]
                 }
-            }
-            return jsonify(response)
-        return jsonify("Ocorreu um erro com a solicitação.")
+
+                handler_input.response_builder.add_directive(
+                    RenderDocumentDirective(
+                        token="uniqueToken",
+                        document=apl_document
+                    )
+                ).add_directive(
+                    ExecuteCommandsDirective(
+                        token="uniqueToken",
+                        commands=apl_command
+                    )
+                ).speak(speech_text).set_should_end_session(False)
+                
+                return handler_input.response_builder.response
+        sb.add_request_handler(LaunchRequestHandler()) 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
         
