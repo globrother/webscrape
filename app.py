e88ddd841083d430637dce619798bc63427c13f9
globrother <clebergobira@yahoo.com.br>
2025-01-01 23:29:56 -0300

    implementando apl

--- app.py
+++ app.py
@@ -1,103 +1,24 @@
 from flask import Flask, request, jsonify
+import json
+import requests
+from bs4 import BeautifulSoup
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type, get_supported_interfaces
 from ask_sdk_model.interfaces.alexa.presentation.apl import RenderDocumentDirective
-import json
+from threading import Timer
+from xpml11_response import alexa_xpml11
+from xpml11 import get_element
+
+# from xpml11 import get_element
+# from xpml11_response import get_element
 
 app = Flask(__name__)
 
-@app.route("/webscrape", methods=["POST"])
-def handle_request():
-    request_data = request.get_json()
+@app.route('/webscrape', methods=['POST'])
+def alexa():
     
-    # Verifica o tipo de solicitação da Alexa
-    if request_data["request"]["type"] == "LaunchRequest":
-        response = {
-            "version": "1.0",
-            "response": {
-                "outputSpeech": {
-                    "type": "SSML",
-                    "ssml": "<speak>Olá, Bem-vindo ao Echo Show!<break time='5s'/></speak>"
-                },
-                "directives": [
-                    {
-                        "type": "Alexa.Presentation.APL.RenderDocument",
-                        "token": "welcomeToken",
-                        "document": {
-                            "type": "APL",
-                            "version": "2024.3",
-                            "license": "Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.\nSPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0\nLicensed under the Amazon Software License  http://aws.amazon.com/asl/",
-                            "theme": "dark",
-                            "import": [
-                                {
-                                    "name": "alexa-layouts",
-                                    "version": "1.7.0"
-                                }
-                            ],
-                            "mainTemplate": {
-                                "parameters": [
-                                    "payload"
-                                ],
-                                "items": [
-                                    {
-                                        "type": "Container",
-                                        "height": "100vh",
-                                        "items": [
-                                            {
-                                                "type": "AlexaBackground",
-                                                "backgroundImageSource": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf",
-                                                "backgroundScale": "best-fill"
-                                            },
-                                            {
-                                                "type": "Container",
-                                                "height": "100vh",
-                                                "width": "100vw",
-                                                "items": [
-                                                    {
-                                                        "type": "AlexaHeader",
-                                                        "headerTitle": "Atualizações Fundos Imobiliários - FIIs",
-                                                        "headerAttributionImage": "${payload.longTextTemplateData.properties.logoUrl}"
-                                                    },
-                                                    {
-                                                        "type": "ScrollView",
-                                                        "paddingTop": "@spacingMedium",
-                                                        "paddingBottom": "${@spacing3XLarge + @spacingXSmall}",
-                                                        "paddingLeft": "@marginHorizontal",
-                                                        "paddingRight": "@marginHorizontal",
-                                                        "grow": 1,
-                                                        "items": [
-                                                            {
-                                                                "type": "Text",
-                                                                "text": "marcadores voz,",
-                                                                "style": "textStyleDisplay4",
-                                                                "textAlign": "left",
-                                                                "speech": "${payload.longTextTemplateData.properties.plantInfoSpeech}",
-                                                                "id": "financeContent"
-                                                            }
-                                                        ]
-                                                    }
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                ]
-                            },
-                            "onMount": [
-                                {
-                                    "type": "SpeakItem",
-                                    "componentId": "financeContent",
-                                    "highlightMode": "line",
-                                    "align": "center"
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-        return jsonify(response)
-    return jsonify({})
+    return alexa_xpml11(get_element, request, requests, BeautifulSoup)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- xpml11_response.py
+++ xpml11_response.py
@@ -7,8 +7,9 @@ from flask import jsonify
 
 def alexa_xpml11(get_element, request, requests, BeautifulSoup):
     try:
-        data = request.get_json()
-        
+        #data = request.get_json()
+        request_data = request.get_json()
+
         xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16 = get_element(requests, BeautifulSoup)
         
         voz_xpml11 = f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
@@ -20,7 +21,99 @@ def alexa_xpml11(get_element, request, requests, BeautifulSoup):
              #f"• Dividend Yield: {dyxpml11_3}%\n" \
              #f"• P/VP: {pvpxpml11_6}\n" \
              #f"• Último rendimento: R$ {divpcxmpl11_16}"
-
+        
+        # Verifica o tipo de solicitação da Alexa
+        if request_data["request"]["type"] == "LaunchRequest":
+            response = {
+                "version": "1.0",
+                "response": {
+                    "outputSpeech": {
+                        "type": "SSML",
+                        "ssml": f"<speak> Atualizações do Fundo XPML onze \n{voz_xpml11}</speak>"
+                    },
+                    "directives": [
+                        {
+                            "type": "Alexa.Presentation.APL.RenderDocument",
+                            "token": "welcomeToken",
+                            "document": {
+                                "type": "APL",
+                                "version": "2024.3",
+                                "license": "Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.\nSPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0\nLicensed under the Amazon Software License  http://aws.amazon.com/asl/",
+                                "theme": "dark",
+                                "import": [
+                                    {
+                                        "name": "alexa-layouts",
+                                        "version": "1.7.0"
+                                    }
+                                ],
+                                "mainTemplate": {
+                                    "parameters": [
+                                        "payload"
+                                    ],
+                                    "items": [
+                                        {
+                                            "type": "Container",
+                                            "height": "100vh",
+                                            "items": [
+                                                {
+                                                    "type": "AlexaBackground",
+                                                    "backgroundImageSource": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf",
+                                                    "backgroundScale": "best-fill"
+                                                },
+                                                {
+                                                    "type": "Container",
+                                                    "height": "100vh",
+                                                    "width": "100vw",
+                                                    "items": [
+                                                        {
+                                                            "type": "AlexaHeader",
+                                                            "headerTitle": "Atualizações Fundos Imobiliários - FIIs",
+                                                            "headerAttributionImage": "${payload.longTextTemplateData.properties.logoUrl}"
+                                                        },
+                                                        {
+                                                            "type": "ScrollView",
+                                                            "paddingTop": "@spacingMedium",
+                                                            "paddingBottom": "${@spacing3XLarge + @spacingXSmall}",
+                                                            "paddingLeft": "@marginHorizontal",
+                                                            "paddingRight": "@marginHorizontal",
+                                                            "grow": 1,
+                                                            "items": [
+                                                                {
+                                                                    "type": "Text",
+                                                                    "text": f"Atualizações do Fundo XPML11:\n\n{card_xpml11}",
+                                                                    "style": "textStyleDisplay4",
+                                                                    "textAlign": "left",
+                                                                    "speech": "${payload.longTextTemplateData.properties.plantInfoSpeech}",
+                                                                    "id": "financeContent"
+                                                                }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                },
+                                "onMount": [
+                                    {
+                                        "type": "SpeakItem",
+                                        "componentId": "financeContent",
+                                        "highlightMode": "line",
+                                        "align": "center"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+            return jsonify(response)
+        return jsonify({})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+        
+        
+        """
         response = {
             "version": "1.0",
             "sessionAttributes":{
@@ -60,7 +153,7 @@ def send_follow_up_response():
     }
     return response
 
-"""
+    
 def send_follow_up_response():
     response = {
         "version": "1.0",
