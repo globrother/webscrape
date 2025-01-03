f39829d1b8753b71aa1b81438b573b35c4421708
globrother <clebergobira@yahoo.com.br>
2025-01-03 01:43:51 -0300

    implementando apl

--- xpml11_response.py
+++ xpml11_response.py
@@ -32,8 +32,8 @@ def alexa_xpml11(json, get_element, request, requests, BeautifulSoup):
         # Verifica o tipo de solicitação da Alexa
         if request_data["request"]["type"] in ["LaunchRequest", "IntentRequest"]:
             response = {
-                "version": "1.0",
-                "response": {
+                #"version": "1.0",
+                #"response": {
                    # "outputSpeech": {
                    #     "type": "SSML",
                    #     "ssml": f"<speak> Atualizações do Fundo XPML onze \n{voz_xpml11}</speak>"
@@ -112,7 +112,7 @@ def alexa_xpml11(json, get_element, request, requests, BeautifulSoup):
                             }
                         }
                     ]
-                }
+                #}
             }
             return jsonify(response)
         return jsonify("Ocorreu um erro com a solicitação.")
