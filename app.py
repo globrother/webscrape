f0a97aa8ef2cb923e4b532629bbeafcad84e933c
globrother <clebergobira@yahoo.com.br>
2025-01-03 01:32:54 -0300

    implementando apl

--- app.py
+++ app.py
@@ -1,149 +1,24 @@
-import logging
+from flask import Flask, request, jsonify
+import json
 import requests
-from flask import Flask, jsonify
+from bs4 import BeautifulSoup
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.utils import is_request_type, get_supported_interfaces
 from ask_sdk_model.interfaces.alexa.presentation.apl import RenderDocumentDirective
+from threading import Timer
+from xpml11_response import alexa_xpml11
+from xpml11 import get_element
 
-# Configuração do logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+# from xpml11 import get_element
+# from xpml11_response import get_element
 
 app = Flask(__name__)
-sb = SkillBuilder()
 
-# Endpoint da API
-ENDPOINT = "https://alexawebscrape.onrender.com/webscrape"
-
-# Handlers da skill Alexa
-class LaunchRequestHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        return is_request_type("LaunchRequest")(handler_input)
-
-    def handle(self, handler_input):
-        logger.info("In LaunchRequestHandler")
-
-        try:
-            response = requests.get(f"{ENDPOINT}/launch")
-            response.raise_for_status()
-            logger.info(f"HTTP Response: {response.text}")
-            data = response.json()
-            logger.info(f"Data: {data}")
-        except requests.exceptions.RequestException as e:
-            logger.error(f"HTTP Request failed: {e}")
-            data = {}
-
-        apl_document = data.get("document", {})
-        datasource = data.get("datasources", {})
-
-        if apl_document and datasource:
-            handler_input.response_builder.add_directive(
-                RenderDocumentDirective(token="mainToken", document=apl_document, datasources=datasource)
-            )
-            speech_text = "Bem-vindo à skill de atualizações de fundos imobiliários!"
-        else:
-            speech_text = "Desculpe, algo deu errado ao tentar obter os dados."
-
-        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
-        return handler_input.response_builder.response
-
-class IntentRequestHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        return is_request_type("IntentRequest")(handler_input)
-
-    def handle(self, handler_input):
-        intent_name = handler_input.request_envelope.request.intent.name
-        logger.info(f"In IntentRequestHandler with intent: {intent_name}")
-
-        try:
-            response = requests.get(f"{ENDPOINT}/intent?name={intent_name}")
-            response.raise_for_status()
-            logger.info(f"HTTP Response: {response.text}")
-            data = response.json()
-            logger.info(f"Data: {data}")
-        except requests.exceptions.RequestException as e:
-            logger.error(f"HTTP Request failed: {e}")
-            data = {}
-
-        speech_text = data.get("speech_text", "Desculpe, eu não entendo essa solicitação.")
-        handler_input.response_builder.speak(speech_text).set_should_end_session(True)
-
-        return handler_input.response_builder.response
-
-sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(IntentRequestHandler())
-
-lambda_handler = sb.lambda_handler()
-
-# Definindo a rota e a função para lidar com a requisição
 @app.route("/webscrape", methods=["POST"])
 def handle_request():
-    return jsonify({
-        "document": {
-            "type": "APL",
-            "version": "2024.3",
-            "import": [
-                {
-                    "name": "alexa-layouts",
-                    "version": "1.7.0"
-                }
-            ],
-            "mainTemplate": {
-                "parameters": ["payload"],
-                "items": [
-                    {
-                        "type": "AlexaPaginatedList",
-                        "id": "paginatedList",
-                        "headerTitle": "${payload.imageListData.title}",
-                        "headerBackButton": True,
-                        "headerAttributionImage": "${payload.imageListData.logoUrl}",
-                        "backgroundBlur": False,
-                        "backgroundColorOverlay": False,
-                        "backgroundScale": "best-fill",
-                        "backgroundAlign": "bottom",
-                        "theme": "dark",
-                        "listItems": "${payload.imageListData.listItems}"
-                    }
-                ]
-            }
-        },
-        "datasources": {
-            "imageListData": {
-                "type": "object",
-                "objectId": "paginatedListSample",
-                "title": "ATUALIZAÇÕES FUNDOS IMOBILIÁRIOS - FIIs",
-                "listItems": [
-                    {
-                        "primaryText": "Fundos Imobiliários",
-                        "secondaryText": "5 itens",
-                        "imageSource": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf"
-                    },
-                    {
-                        "primaryText": "Fundo XML11",
-                        "secondaryText": "5 items",
-                        "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark2.png"
-                    },
-                    {
-                        "primaryText": "Fundo MXRF11",
-                        "secondaryText": "5 items",
-                        "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark3.png"
-                    },
-                    {
-                        "primaryText": "Fundo KNRI11",
-                        "secondaryText": "5 items",
-                        "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark4.png"
-                    },
-                    {
-                        "primaryText": "Fundo XPLG11",
-                        "secondaryText": "5 items",
-                        "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark5.png"
-                    }
-                ],
-                "logoUrl": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/logo/logo-modern-botanical-white.png"
-            }
-        }
-    })
+    
+    return alexa_xpml11(json,get_element, request, requests, BeautifulSoup)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- xpml11_response.py
+++ xpml11_response.py
@@ -5,170 +5,116 @@ from flask import jsonify
 #import time
 #from xpml11 import get_element
 
-#def alexa_xpml11(sb, ExecuteCommandsDirective, RenderDocumentDirective, AbstractRequestHandler, json, get_element, request, requests, BeautifulSoup):
-   # try:
+def alexa_xpml11(json, get_element, request, requests, BeautifulSoup):
+    try:
         #data = request.get_json()
-        #request_data = request.get_json()
+        request_data = request.get_json()
 
         # Adiciona instrução de depuração para imprimir o conteúdo da solicitação
         # print("Request Data:", json.dumps(request_data, indent=2))
 
-        #xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16 = get_element(requests, BeautifulSoup)
+        xpml11_0, dyxpml11_3, pvpxpml11_6, divpcxmpl11_16 = get_element(requests, BeautifulSoup)
         
-        #voz_xpml11 = (
-            #f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
-            #f"• Dividend Yield: {dyxpml11_3}%\n<break time='500ms'/>"
-            #f"• PVP: {pvpxpml11_6}\n<break time='500ms'/>"
-            #f"• Último rendimento: R$ {divpcxmpl11_16}<break time='1s'/>"
-        #)
+        voz_xpml11 = (
+            f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
+            f"• Dividend Yield: {dyxpml11_3}%\n<break time='500ms'/>"
+            f"• P/VP: {pvpxpml11_6}\n<break time='500ms'/>"
+            f"• Último rendimento: R$ {divpcxmpl11_16}<break time='1s'/>"
+        )
         
-        #card_xpml11 = (
-           # f"• Valor atual da cota: R$ {xpml11_0}<br>"
-           # f"• Dividend Yield: {dyxpml11_3}%<br>"
-          #  f"• P/VP: {pvpxpml11_6}<br>"
-          #  f"• Último rendimento: R$ {divpcxmpl11_16}"
-        #)
-
-       # voz_teste1 = (
-          #  f"• Valor atual da cota: R$ {xpml11_0}\n<break time='500ms'/>"
-       # )
-
-        #card_teste1 = (
-        #    "• Valor atual da cota: R$ 5.000,00<br>"
-        #)
-
+        card_xpml11 = (
+            f"• Valor atual da cota: R$ {xpml11_0}<br>"
+            f"• Dividend Yield: {dyxpml11_3}%<br>"
+            f"• P/VP: {pvpxpml11_6}<br>"
+            f"• Último rendimento: R$ {divpcxmpl11_16}"
+        )
+        
         # Verifica o tipo de solicitação da Alexa
-        class LaunchRequestHandler(AbstractRequestHandler):
-            def can_handle(self, handler_input):
-                return handler_input.request_envelope.request.object_type == "LaunchRequest"
-
-            def handle(self, handler_input):
-                speech_text = "Bem-vindo! Aqui estão os cartões."
-                
-                apl_document = {
-                    "type": "APL",
-                    "version": "1.4",
-                    "mainTemplate": {
-                        "items": [
-                            {
-                                "type": "Container",
-                                "items": [
+        if request_data["request"]["type"] in ["LaunchRequest", "IntentRequest"]:
+            response = {
+                "version": "1.0",
+                "response": {
+                    "outputSpeech": {
+                        "type": "SSML",
+                        "ssml": f"<speak> Atualizações do Fundo XPML onze \n{voz_xpml11}</speak>"
+                    },
+                    "directives": [
+                        {
+                            "type": "Alexa.Presentation.APL.RenderDocument",
+                            "token": "welcomeToken",
+                            "document": {
+                                "type": "APL",
+                                "version": "2024.3",
+                                "license": "Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.\nSPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0\nLicensed under the Amazon Software License  http://aws.amazon.com/asl/",
+                                "theme": "dark",
+                                "import": [
                                     {
-                                        "type": "Image",
-                                        "source": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf",
-                                        "width": "100vw",
-                                        "height": "100vh"
-                                    },
-                                    {
-                                        "type": "Text",
-                                        "text": "Primeiro cartão",
-                                        "fontSize": "50dp",
-                                        "color": "#FFFFFF",
-                                        "textAlign": "center",
-                                        "textAlignVertical": "center"
+                                        "name": "alexa-layouts",
+                                        "version": "1.7.0"
                                     }
-                                ]
+                                ],
+                                "mainTemplate": {
+                                    "parameters": [
+                                        "payload"
+                                    ],
+                                    "items": [
+                                        {
+                                            "type": "AlexaPaginatedList",
+                                            "id": "paginatedList",
+                                            "headerTitle": "${payload.imageListData.title}",
+                                            "headerBackButton": True,
+                                            "headerAttributionImage": "${payload.imageListData.logoUrl}",
+                                            "backgroundBlur": False,
+                                            "backgroundColorOverlay": False,
+                                            "backgroundScale": "best-fill",
+                                            "backgroundAlign": "bottom",
+                                            "theme": "dark",
+                                            "listItems": "${payload.imageListData.listItems}"
+                                        }
+                                    ]
+                                },
+                                "datasources": {
+                                    "imageListData": {
+                                        "type": "object",
+                                        "objectId": "paginatedListSample",
+                                        "title": "ATUALIÇÕES FUNDOS IMOBILIÁRIOS - FIIs",
+                                        "listItems": [
+                                            {
+                                                "primaryText": "Fundos Imobiliários",
+                                                "secondaryText": "5 itens",
+                                                "imageSource": "https://lh5.googleusercontent.com/d/1QZIOOt7ziy5avs2FklbSFoJxhUFpXFYf"
+                                            },
+                                            {
+                                                "primaryText": "Fundo XML11",
+                                                "secondaryText": "5 items",
+                                                "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark2.png"
+                                            },
+                                            {
+                                                "primaryText": "Fundo MXRF11",
+                                                "secondaryText": "5 items",
+                                                "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark3.png"
+                                            },
+                                            {
+                                                "primaryText": "Fundo KNRI11",
+                                                "secondaryText": "5 items",
+                                                "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark4.png"
+                                            },
+                                            {
+                                                "primaryText": "Fundo XPLG11",
+                                                "secondaryText": "5 items",
+                                                "imageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/paginatedlist/PaginatedList_Dark5.png"
+                                            }
+                                        ],
+                                        "logoUrl": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/logo/logo-modern-botanical-white.png"
+                                    }
+                                },
+                                "sources": {}
                             }
-                        ]
-                    }
-                }
-                
-                apl_command = {
-                    "type": "Sequential",
-                    "commands": [
-                        {
-                            "type": "SpeakItem",
-                            "componentId": "textComponent",
-                            "highlightMode": "line"
-                        },
-                        {
-                            "type": "SetValue",
-                            "componentId": "textComponent",
-                            "property": "text",
-                            "value": "Segundo cartão"
-                        },
-                        {
-                            "type": "SpeakItem",
-                            "componentId": "textComponent",
-                            "highlightMode": "line"
                         }
                     ]
                 }
-
-                handler_input.response_builder.add_directive(
-                    RenderDocumentDirective(
-                        token="uniqueToken",
-                        document=apl_document
-                    )
-                ).add_directive(
-                    ExecuteCommandsDirective(
-                        token="uniqueToken",
-                        commands=apl_command
-                    )
-                ).speak(speech_text).set_should_end_session(False)
-                
-                return handler_input.response_builder.response
-        sb.add_request_handler(LaunchRequestHandler()) 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-        
-        
-        """
-        response = {
-            "version": "1.0",
-            "sessionAttributes":{
-                 "follow_up": True },
-            "response": {
-                "outputSpeech": {
-                    "type": "SSML",
-                    "ssml": f"<speak> Atualizações do Fundo XPML onze \n{voz_xpml11}</speak>"
-                },
-                "card": {
-                    "type": "Simple",
-                    "title": "Obtendo de Status Invest",
-                    "content": f"Atualizações do Fundo XPML11:\n\n{card_xpml11}"
-                },
-                "shouldEndSession": False # Mantem a sessão aberta para a próxima atualização
             }
-        }
-        return jsonify(response)
+            return jsonify(response)
+        return jsonify("Ocorreu um erro com a solicitação.")
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-def send_follow_up_response():
-    response = {
-        "version": "1.0",
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "Aqui está a próxima atualização."
-            },
-            "card": {
-                "type": "Simple",
-                "title": "Mais Informações do Fundo XPML11",
-                "content": "Informações adicionais aqui."
-            },
-            "shouldEndSession": True  # Fecha a sessão após a resposta
-        }
-    }
-    return response
-
-    
-def send_follow_up_response():
-    response = {
-        "version": "1.0",
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "Aqui está a próxima atualização."
-            },
-            "card": {
-                "type": "Simple",
-                "title": "Mais Informações do Fundo XPML11",
-                "content": "Informações adicionais aqui."
-            },
-            "shouldEndSession": True  # Fecha a sessão após a resposta
-        }
-    }
-    return jsonify(response)
-"""
