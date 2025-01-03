d68449d3e3db19a90c24b475585a5db9aaa59404
globrother <clebergobira@yahoo.com.br>
2025-01-03 11:05:07 -0300

    implementando apl

---
+++ app - Copia.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import json
+import requests
+from bs4 import BeautifulSoup
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_request_type, get_supported_interfaces
+from ask_sdk_model.interfaces.alexa.presentation.apl import RenderDocumentDirective
+from threading import Timer
+from xpml11_response import alexa_xpml11
+from xpml11 import get_element
+
+# from xpml11 import get_element
+# from xpml11_response import get_element
+
+app = Flask(__name__)
+
+@app.route("/webscrape", methods=["POST"])
+def handle_request():
+    
+    return alexa_xpml11(json,get_element, request, requests, BeautifulSoup)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ data.py
@@ -0,0 +1,19 @@
+# data.py
+
+# Mensagens de boas-vindas
+WELCOME_MESSAGE = "Bem-vindo ao meu skill Alexa! Como posso ajudar você hoje?"
+
+# Mensagens para a intent Hello World
+HELLO_MSG = "Olá, Mundo!"
+
+# Mensagens de ajuda
+HELP_MSG = "Você pode me pedir para dizer Olá Mundo ou perguntar sobre outra coisa."
+
+# Mensagens de despedida
+GOODBYE_MSG = "Até logo! Foi um prazer ajudar você."
+
+# Mensagens de erro
+ERROR = "Desculpe, algo deu errado. Por favor, tente novamente mais tarde."
+
+# Mensagens de refletor de intent
+REFLECTOR_MSG = "Você acabou de acionar a intent {0}."
