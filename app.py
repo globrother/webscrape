cc1d65559a7d8be5bfbc26cb682e72b73dd53c70
globrother <clebergobira@yahoo.com.br>
2025-01-09 20:36:54 -0300

    apl touch 6 cards v7

--- app.py
+++ app.py
@@ -379,9 +379,9 @@ class CatchAllRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # Em vez de encerrar, vamos definir uma mensagem padrão
+        handler_input.response_builder.speak("Encerrando a skill. Até a próxima!").set_should_end_session(True)
         os._exit(0) # Finalizar servidor Flask
-        return handler_input.response_builder.speak(
-            "Encerrando a skill. Até a próxima!").set_should_end_session(True).response
+        return handler_input.response_builder.response
 
 #class CatchAllRequestHandler(AbstractRequestHandler):
     #def can_handle(self, handler_input):
