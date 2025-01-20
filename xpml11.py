56adb4df16e14baf149da5a20294d9c3f7206719
globrother <clebergobira@yahoo.com.br>
2025-01-20 18:23:57 -0300

    apl touch 6 cards v9

--- xpml11.py
+++ xpml11.py
@@ -28,7 +28,15 @@ def get_xpml(requests, BeautifulSoup):
     try:
         url = 'https://statusinvest.com.br/fundos-imobiliarios/xpml11'
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'DNT': '1',
+            'Referer': 'https://statusinvest.com.br/',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache'
         }
         
         proxies = {
