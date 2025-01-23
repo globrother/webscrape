aeeb097ac575845b21944eee2c011cf69adbcd79
globrother <clebergobira@yahoo.com.br>
2025-01-22 23:43:53 -0300

    apl touch 6 cards v9

--- replit.nix
+++ replit.nix
@@ -3,6 +3,7 @@
     pkgs.python310
     pkgs.python310Packages.gunicorn
     pkgs.python310Packages.flask # Ou qualquer outra biblioteca que você esteja usando
+    pkgs.python310Packages.requests
     # Adicione outras dependências aqui
   ];
 }
