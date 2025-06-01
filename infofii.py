dfa8637b1a65dd4ef02918739936fc852c57105e
globrother <clebergobira@yahoo.com.br>
2025-05-31 23:37:34 -0300

    codigo reduzido v14.2

--- infofii.py
+++ infofii.py
@@ -100,7 +100,7 @@ def get_dadosfii(fii):
             elif tipo_ativo == "acao":
                 logging.info(f"\nRASPANDO ATIVO:> {tipo_ativo}\n")
                 containers = soup.find_all('div', class_='container')
-                container = containers[4]
+                container = containers[5]
                 if not container:
                     logger.info(
                         "Container principal NÃO encontrado para ação.")
