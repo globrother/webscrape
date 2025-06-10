23042164f3a35bf595aa447062425ce09adecbee
globrother <clebergobira@yahoo.com.br>
2025-06-10 01:14:07 -0300

    codigo reduzido v16 chart

---
+++ obter_grafico.py
@@ -0,0 +1,23 @@
+import requests
+import os
+
+API_URL = "http://204.216.158.137:8000/screenshot"
+CHART_API_KEY = os.getenv("CHART_API_KEY")
+
+def requisitando_chart(ticker):
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": CHART_API_KEY
+    }
+    data = {"ticker": ticker}
+
+    response = requests.post(API_URL, json=data, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()  # Retorna a resposta da API
+    else:
+        return {"error": f"Erro {response.status_code}: {response.text}"}
+
+# 🔹 Exemplo de chamada
+resultado = requisitando_chart("BBAS3")
+print(resultado)
