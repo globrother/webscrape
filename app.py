a19b7a0803da34a338efcfadf967b78de95a6468
globrother <clebergobira@yahoo.com.br>
2025-06-28 14:46:15 -0300

    codigo reduzido v21 excluirIntent

--- app.py
+++ app.py
@@ -272,6 +272,7 @@ class GerenciarAtivoInputHandler(APLUserEventHandler):
                 sigla = None
                 fala = "Recomeçar"
                 session_attr.pop("novo_ativo_sigla", None)
+                session_attr.pop("tipo_acao", None)
             else:
                 fala = f"O ativo {sigla.upper()} não foi encontrado. Se preferir, toque em cadastrar para incluir o ativo em sua lista."
             
