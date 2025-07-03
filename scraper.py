8d9511da2820807ede9428872b244f5d85da8444
globrother <clebergobira@yahoo.com.br>
2025-07-03 15:32:20 -0300

    codigo reduzido v21 excluirIntent

--- scraper.py
+++ scraper.py
@@ -75,8 +75,8 @@ def web_scrape(fundo):
     # DIVIDE O HISTÓRICO EM DUAS COLUNAS
     #log_info(f"hist_text_FII é: {hist_text_fii}")
     meio = len(hist_text_fii) // 2  # Divide a lista ao meio
-    hist_text_ativo_col1 = hist_text_fii[:meio]  # Primeira metade da lista
-    hist_text_ativo_col2 = hist_text_fii[meio:]   # Segunda metade da lista
+    hist_text_ativo_col1 = hist_text_fii[:meio][:20]  # Primeira metade da lista
+    hist_text_ativo_col2 = hist_text_fii[meio:][:20]   # Segunda metade da lista
     #log_info(f"COl2 é: {hist_text_ativo_col2}")
 
     dados_info = {
