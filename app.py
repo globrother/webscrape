9b4f558020ac9d09b5ff983216ca68aaf09f9f4a
globrother <clebergobira@yahoo.com.br>
2025-08-07 21:08:44 -0300

    codigo reduzido v21 revisao

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
             (13, 14),  # exemplo: das 13h às 14h
             (15, 16),
             (17, 18),
-            (21, 24),
+            (22, 24),
             (00, 8)
             # adicione outros intervalos conforme desejar
         ]
