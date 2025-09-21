d5fe24579d345755ec519bfd2e71c88cbb7666b2
globrother <clebergobira@yahoo.com.br>
2025-09-21 12:59:05 -0300

    codigo migracao v1

--- chart_server.py
+++ chart_server.py
@@ -67,7 +67,7 @@ def gerar_grafico(ticker):
     # 🔹 Obtendo dados do yfinance
     try:
         print (f"requisitando Yahoo Finance para {ticker}")
-        ativo = yf.Ticker(ticker)
+        ativo = yf.Ticker(XPML11.SA)
         historico = ativo.history(period="100d")
         print("requisição finalizada para um peíriodo de 100 dias.")
         if historico.empty:
