d808fd63273fe0b82af9ccee4d950d116e9df63b
globrother <clebergobira@yahoo.com.br>
2025-09-21 01:03:54 -0300

    codigo migracao v1

--- app.py
+++ app.py
@@ -1884,4 +1884,4 @@ def webhook():
 
 if __name__ == '__main__':
     log_info("\n Iniciando o servidor Flask...\n")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
