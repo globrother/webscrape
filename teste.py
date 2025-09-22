ac419c6233772177dff57125f7def4c7f61eda97
globrother <clebergobira@yahoo.com.br>
2025-09-22 13:21:17 -0300

    codigo migracao v1

--- teste.py
+++ teste.py
@@ -2,5 +2,5 @@ import plotly.express as px
 
 fig = px.line(x=[1, 2, 3], y=[10, 20, 30])
 print("chamando fig.write_image")
-fig.write_image("teste.png")
+fig.write_html(".png", ".html")
 print("finalizando")
